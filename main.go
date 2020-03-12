517db10f78a79b9e705dbc77f507e7e00e152122
Preetam Jinka <pj@preet.am>
2020-03-11 23:40:00 -0700

    updates

--- main.go
+++ main.go
@@ -8,10 +8,18 @@ import (
 
 func main() {
 	listen := flag.String("listen", "127.0.0.1:8000", "Listen address")
+	devMode := flag.Bool("dev-mode", false, "Enables developer mode")
 	flag.Parse()
 
 	fs := http.FileServer(http.Dir("ui"))
-	http.Handle("/", fs)
+	handler := fs
+	if *devMode {
+		handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Add("access-control-allow-origin", "*")
+			fs.ServeHTTP(w, r)
+		})
+	}
+	http.Handle("/", handler)
 	log.Println("Listening on", *listen)
 	log.Fatal(http.ListenAndServe(*listen, nil))
 }
